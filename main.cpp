69f62e0b4d00b2b5e8e98851228721bcc7f84d38
doki-JP <111583401+doki-JP@users.noreply.github.com>
2023-11-27 11:00:14 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,30 @@
 //Añadir una introducción tipo videojuego, ej. bienvenido, hoy haremos tu inventario.... (debe ir más allá en cuanto a funcionalidad)
 int main() {
     std::string name;
-    int cant, type;
-    int o1;
+    int cant, type, orden, menu;
     int opcion=1;
     ItemList<int> Inventory;
     Inventory.addFirst("Brujula", 1,0);
 
     /*-------Menú-------*/
-    
     while (opcion!=0){
         std::cout<<"\nInventario: "<<std::endl;
         std::cout<<"\t 0. Cerrar\n\t 1. Mostrar Inventario completo\n\t 2. Agregar Item\n\t 3. Ordenar"<<std::endl;
         std::cin>>opcion;
         if (opcion==1)
         {
-            Inventory.print();
+            std::cout<<"\t1. Imprimir al derecho\n\t2. Imprimir al reves\n";
+            std::cin>>menu;
+            if (menu==1){
+                Inventory.print();    
+            }
+            else if(menu==2){
+                Inventory.printContrario();
+            }
+            else{
+                std::cout<<"Opcion no valida, regresando al menu...";
+            }
+            
         }
         else if (opcion==2)
         {
@@ -33,7 +42,9 @@ int main() {
         }
         else if (opcion ==3)
         {
-            Inventory.Insertionsort();
+            std::cout<<"A partir de que atributo lo quieres ordenar?\n\t1.Nombre (Alfabéticamente)\n\t2. Cantidad\n\t3. Tipo\n Selecccion: ";
+            std::cin>>orden;
+            Inventory.Sort();
 
         }
         else if (opcion>3||opcion<0)
